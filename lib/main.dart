6ff37e1e3ab182497351805aad26ac81cebb9799
donghyuk <udhyk27@gmail.com>
2026-03-19 14:58:21 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:image_picker/image_picker.dart';
 
 import 'screens/history_screen.dart';
